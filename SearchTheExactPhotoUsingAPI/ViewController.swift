40711c0d44ebe3c7764b05446ee49ecef477c7e8
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-14 12:42:56 +0300

    data fetched successfully.

--- SearchTheExactPhotoUsingAPI/ViewController.swift
+++ SearchTheExactPhotoUsingAPI/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let urlString = "https://api.unsplash.com/search/photos?page=1&per_page=50&query=office&client_id=8v55atTFhjnu3fr-PT0cEYO8i86q5gf4mq_9W0yiF80"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchPhotos()
+    }
+    
+    func fetchPhotos() {
+        
+        guard let url = URL(string: urlString) else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else {return}
+            
+            print("Got Data")
+        }
+        task.resume()
+        
     }
 
 
